4ea18b791b6b0b379d5e0e603e0d751d179a04a3
vparakala <vparakala@berkeley.edu>
2019-08-27 21:59:38 +0000

    add cron job for updatestocks

--- modules/updateStocks.py
+++ modules/updateStocks.py
@@ -4,13 +4,15 @@ import datetime
 import hashlib
 import pymongo
 import time
+import argparse
 
 
-def updateStock(ticker, hours_back, interval=5):
+def updateStock(ticker, hours_back, interval=5, insert=False):
     api_call = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=%dmin&outputsize=full&apikey=%s' % (ticker, interval, constants['alpha_vantage_api_key'])
     response = requests.get(url=api_call)
     price_data = None
-    price_data_list = []
+    if insert:
+        price_data_list = []
     try:
         price_data = response.json()['Time Series (%dmin)' % (interval)]
     except KeyError:
@@ -37,25 +39,42 @@ def updateStock(ticker, hours_back, interval=5):
             price_data_dict['timestamp'] = timestamp_s
             price_data_dict['date'] = timestamp_s[:10]
             price_data_dict['price'] = 0.5 * round((float(price_data[timestamp_s]['2. high'])+float(price_data[timestamp_s]['3. low'])), 2)
-            price_data_list.append(price_data_dict)
+            if insert:
+                price_data_list.append(price_data_dict)
+            else:
+                stock_data_collection.update({'_id':id}, {'$set': price_data_dict}, upsert=True)
+    if insert:
+        stock_data_collection.insert_many(price_data_list)
 
-            #stock_data_collection.update({'_id':id}, {'$set': price_data_dict}, upsert=True)
-    stock_data_collection.insert_many(price_data_list)
 
+def insertStock(ticker, hours_back, interval=5):
+    stock_db = constants['db_client'].get_database('stocktwits_db').all_stocks
+    try:
+        updateStock(ticker, hours_back, interval, True) 
+    except KeyError:
+        return -1
+    except pymongo.errors.BulkWriteError:
+        print('Stock data already exists. Try updateStock instead')
+    stock_db.insert({'_id':ticker})
+    return 1
 
+    
 def updateAllStocks(hours_back=24, interval=5):
     all_tickers = constants['db_client'].get_database('stocktwits_db').all_stocks.find({})
     for t in all_tickers:
-        if t['_id'] in already_written:
-            continue
         print('Updating stock data for ticker %s' % (t['_id']))
-        updateStock(t['_id'], hours_back, interval)
-        written_file.write(t['_id']+'\n')
+        try:
+            updateStock(t['_id'], hours_back, interval)
+        except KeyError:
+            print('Ticker %s does not exist' % (interval))
 
-already_written = set()
-with open('already_written.txt', 'r') as f:
-    for line in f:
-        already_written.add(line.strip())
-written_file = open('already_written.txt', 'a+')
-updateAllStocks(hours_back=24*35)
-written_file.close()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', action='store_true', dest='update', default=False)
+    results = parser.parse_args()
+    if results.update:
+        updateAllStocks()
+    
+
+if __name__ == '__main__':
+    main()
