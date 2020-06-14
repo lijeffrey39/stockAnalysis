1b1d18b16208a1ccec505df98ab25347b4f05520
bparambath <bparambath1@gmail.com>
2020-06-13 20:16:39 -0400

    yf and iex for findCO

--- stocktwits.py
+++ stocktwits.py
@@ -344,14 +344,17 @@ def main():
 
         # check last parsetime
         # stocks = getTopStocks(100)
-        # stocks1 = getSortedStocks()[101:551]
+        # # stocks1 = getSortedStocks()[101:551]
 
         # db = constants['stocktweets_client'].get_database('stocks_data_db')
         # lastParsed = db.last_parsed
         # for i in stocks:
         #     print(i)
         #     lastTime = lastParsed.find({'_id': i})
-        #     print(str(i) + ':' + str(lastTime[0]))
+        #     try:
+        #         print(str(i) + ':' + str(lastTime[0]))
+        #     except:
+        #         pass
 
         # db = clientStockTweets.get_database('stocks_data_db')
         # errors = db.stock_tweets_errors.find()
